23eacde96fcca28556a5534bfaeb90e39ff95520
Golam Rahman Sagor <grsagor08@gmail.com>
2023-11-23 09:07:52 +0600

    push

--- resources/views/frontend/include/header.blade.php
+++ resources/views/frontend/include/header.blade.php
@@ -7,10 +7,14 @@
 
         <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
-                <li><a class="{{ request()->routeIs('frontend.home') ? 'active' : '' }}" href="{{ route('frontend.home') }}">Home</a></li>
-                <li><a class="{{ request()->routeIs('frontend.about.us') ? 'active' : '' }}" href="{{ route('frontend.about.us') }}">About Us</a></li>
-                <li><a class="{{ request()->routeIs('frontend.menu') ? 'active' : '' }}" href="{{ route('frontend.menu') }}">Menu</a></li>
-                <li><a class="{{ request()->routeIs('frontend.contact.us') ? 'active' : '' }}" href="{{ route('frontend.contact.us') }}">Contact Us</a></li>
+                <li><a class="{{ request()->routeIs('frontend.home') ? 'active' : '' }}"
+                        href="{{ route('frontend.home') }}">Home</a></li>
+                <li><a class="{{ request()->routeIs('frontend.about.us') ? 'active' : '' }}"
+                        href="{{ route('frontend.about.us') }}">About Us</a></li>
+                <li><a class="{{ request()->routeIs('frontend.menu') ? 'active' : '' }}"
+                        href="{{ route('frontend.menu') }}">Menu</a></li>
+                <li><a class="{{ request()->routeIs('frontend.contact.us') ? 'active' : '' }}"
+                        href="{{ route('frontend.contact.us') }}">Contact Us</a></li>
             </ul>
             <i class="fa-solid fa-bars mobile-nav-toggle"></i>
         </nav>
@@ -25,3 +29,17 @@
 
     </div>
 </header>
+<iframe src="{{ asset('assets\audio\silence.mp3') }}" type="audio/mp3" allow="autoplay" id="audio"
+    style="display:none"></iframe>
+<audio id="myAudio" controls preload="auto" autoplay loop>
+    <source src="{{ asset('assets\audio\ZORBA-ORIGINAL.mp3') }}" type="audio/mp3">
+</audio>
+<button style="margin: 100px;" onclick="PlayStop()">Play Audio</button>
+<button style="margin: 100px;" onclick="pauseAudio()">Pause Audio</button>
+<script>
+    var myaudio = document.getElementById("myAudio");
+    function PlayStop() {
+        return myaudio.paused ? myaudio.play() : myaudio.pause();
+    };
+</script>
+    
